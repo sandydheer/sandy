ec2a30882576dbd636d182226f56583ccd092b77
sandydheer <141130572+sandydheer@users.noreply.github.com>
2025-12-16 12:29:44 +0530

    Add files via upload

---
+++ assign1_task1.py
@@ -0,0 +1,17 @@
+"""
+Task 1
+Addition
+Subtraction
+Multiply
+Division
+
+"""
+
+amt1 = int(input("Enter First Number  : "))
+amt2 = int(input("Enter Second Number : "))
+
+print("Addition:       ",amt1 + amt2)
+print("Substraction:   ",amt1 - amt2)
+print("Multiplication: ",amt1 * amt2)
+print("Division:       ",float(amt1 / amt2))
+
---
+++ assign1_task2.py
@@ -0,0 +1,9 @@
+"""
+Task 2
+Personalised Message
+"""
+
+first_name = input("Enter your First Name: ")
+last_name = input("Enter your Last Name: ")
+
+print("Hello, ",first_name," ",last_name ,"! Welcome to the Python program.",sep="")
